dff44f9248104665c80a9481b815ef7be173f80e
Nethravathibs123 <nethranethra451@gmail.com>
2024-11-18 05:40:29 +0530

    first commit

--- public/signup.js
+++ public/signup.js
@@ -15,12 +15,57 @@ signUpForm.addEventListener('submit', async(event) => {
     const password = document.getElementById('password').value;
   
     try{
-        const response = await axios.post(`http://localhost:4000/user/signup`,{username, email, password});
+        const response = await axios.post(`http://localhost:3000/user/signup`,{username, email, password});
         users.push(response.data);
 
         document.getElementById('username').value = "";
         document.getElementById('email').value = "";
-        document.getElementById('password').value = "";
+        document.getElementById('password').va
+        const signUpForm = document.getElementById("sign-up-form");
+        
+        const errorMsg = document.getElementById('error');
+        
+        let users = [];
+        
+        signUpForm.addEventListener('submit', async(event) => {
+            event.preventDefault();
+            const username = document.getElementById('username').value;
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+          
+            try{
+                const response = await axios.post(`http://localhost:3000/user/signup`,{username, email, password});
+                users.push(response.data);
+        
+                document.getElementById('username').value = "";
+                document.getElementById('email').value = "";
+                document.getElementById('password').value = "";
+                
+        
+                if (response.status === 201) {
+                    document.getElementById('message').textContent = 'User registered successfully.';
+                  //  window.location.href = "/expense.html";
+                } else if (response.status === 409) {
+                    document.getElementById('message').textContent = 'User already exists.';
+                } else {
+                    document.getElementById('message').textContent = 'An error occurred.';
+                }
+                errorMsg.textContent = '';
+            }
+            catch(error) {
+                document.getElementById('username').value = "";
+                document.getElementById('email').value = "";
+                document.getElementById('password').value = "";
+                
+                if (error.response && error.response.data && error.response.data.message) {
+                    errorMsg.textContent = `Error: ${error.response.data.message}`;
+                } else {
+                    console.log('Error adding user:', error);
+                    
+                }
+            }
+            
+        });lue = "";
 
         errorMsg.textContent = '';
     }
